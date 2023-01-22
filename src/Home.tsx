684284b94af03c0ded873bab0b6da1a93b65e831
Cristy Gonzales <cristyg2025@gmail.com>
2023-01-22 11:44:09 -0800

    Updated toaster styling

--- src/Home.tsx
+++ src/Home.tsx
@@ -6,12 +6,15 @@ const Home = () => {
       <button
         onClick={() =>
           toastie({
+            title: "title",
             message: "hi",
-
+            position: "top",
+            positionOffset: 50,
+            side: "left",
+            sideOffset: 50,
           })
         }
       >
-        {" "}
         click me
       </button>
     </div>
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,40 @@
 import "./index.css";
 type ToastieOptions = {
-  direction?: "top" | "bottom" | "right" | "left";
+  title: string;
   message: string;
   timeout?: number;
   theme?: "danger" | "warning" | "success" | "info";
+  position?: "top" | "bottom";
+  positionOffset?: number;
+  side?: "left" | "right";
+  sideOffset?: number;
 };
 
 export const toastie = (opts: ToastieOptions): void => {
-  const { direction = "top", theme = "info", message, timeout = 1500 } = opts;
+  const {
+    title,
+    theme = "info",
+    message,
+    timeout = 2000,
+    position = "top",
+    positionOffset = 0,
+    side = "right",
+    sideOffset = 0,
+  } = opts;
+
   const toastieDiv = document.createElement("div");
-  toastieDiv.textContent = message;
+  const toastieTitle = document.createTextNode(message);
+  toastieDiv.textContent = title;
+
+  toastieDiv.classList.add("t-toaster");
+  toastieDiv.classList.add(`t-position__${position}`);
+  toastieDiv.classList.add(`t-side__${side}`);
   toastieDiv.classList.add(`t-${theme}`);
+  toastieDiv.style[position] = `${positionOffset}px`;
+  toastieDiv.style[side] = `${sideOffset}px`;
 
   document.body.appendChild(toastieDiv);
+  toastieDiv.appendChild(toastieTitle);
 
   setTimeout(() => {
     document.body.removeChild(toastieDiv);
